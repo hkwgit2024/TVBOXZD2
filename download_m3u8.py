2ec746f6ad91efe40834a7de9f4745d52bbcd3b2
qjlxg <qjlxg@163.com>
2025-06-07 21:39:17 +0800

    更新 download_m3u8.py

--- download_m3u8.py
+++ download_m3u8.py
@@ -97,7 +97,7 @@ def parse_m3u_content(content, playlist_index, base_url=None, playlist_name=None
     stream_count = 0
     m3u_name = None
     is_vod = '#EXT-X-PLAYLIST-TYPE:VOD' in content
-    max_channels = 100
+    max_channels = 50
 
     for line in lines:
         if stream_count >= max_channels:
