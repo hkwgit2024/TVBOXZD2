056a9fbae728eb3374bffd9abf708574c407ad28
qjlxg <qjlxg@163.com>
2025-06-10 17:35:32 +0800

    Update mining_check.py

--- subscribe/mining/mining_check.py
+++ subscribe/mining/mining_check.py
@@ -340,10 +340,10 @@ if "__name__==__main__":#主程序开始的地方
     #INFO_YAML = './2.yaml'
     #CONFIG_YAML = './_1.yaml'
     #TG_SOURCE_PATH = './TG.yaml'
-    INFO_YAML = './subscribe/mining/sub_info.yaml'
-    INFO_YAML_WEB = './subscribe/mining/sub_info.txt' # 如果你希望txt文件也在这里
-    CONFIG_YAML = './subscribe/mining/mining_config.yaml'
-    TG_SOURCE_PATH = './subscribe/mining/TGsources.yaml'
+    INFO_YAML = './sub_info.yaml'
+    INFO_YAML_WEB = './sub_info.txt'
+    CONFIG_YAML = './mining_config.yaml'
+    TG_SOURCE_PATH = './TGsources.yaml'
 
     subs = []
     
