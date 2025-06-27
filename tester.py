ca2ffa9c67b69a11aa6bd5ed89b8cbe0b8e42794
qjlxg <qjlxg@163.com>
2025-06-27 11:25:33 +0800

    Update tester.py

--- tester.py
+++ tester.py
@@ -15,7 +15,8 @@ CLASH_BASE_CONFIG_URLS = [
    # "https://raw.githubusercontent.com/qjlxg/collectSub/refs/heads/main/config_all_merged_nodes.txt",
    # "https://raw.githubusercontent.com/qjlxg/collectSub/refs/heads/main/all_nodes.txt",
    # "https://raw.githubusercontent.com/dimzon/scaling-sniffle/refs/heads/main/all-ip-resolve-sort.txt",
-   "https://raw.githubusercontent.com/qjlxg/TV/refs/heads/main/20232.txt",
+  # "https://raw.githubusercontent.com/qjlxg/TV/refs/heads/main/20232.txt",
+   "https://raw.githubusercontent.com/qjlxg/vt/refs/heads/main/data/sub_3.txt",
                          ]
 
 def parse_node_link_to_clash_proxy(link: str, index: int = 0) -> dict | None:
