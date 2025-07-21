d253dde53d36ecc2c2e80ec55ead2e3cda4850a5
qjlxg <qjlxg@163.com>
2025-07-21 11:53:54 +0800

    Update convert_to_yaml.py

--- convert_to_yaml.py
+++ convert_to_yaml.py
@@ -12,8 +12,7 @@ from urllib3.util.retry import Retry
 urls = [
     "https://raw.githubusercontent.com/qjlxg/ss/refs/heads/master/list.meta.yml",
     "https://raw.githubusercontent.com/qjlxg/hy2/refs/heads/main/configtg.txt",
-    "https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/ss.txt",
-    "https://raw.githubusercontent.com/qjlxg/collectSub/refs/heads/main/config_all_merged_nodes.txt"
+
 ]
 
 # Clash/Mihomo 配置模板
