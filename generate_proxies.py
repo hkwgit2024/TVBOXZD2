24b5b0f628cb76105cfb09e6bcafb324c0e1290e
qjlxg <qjlxg@163.com>
2025-08-23 09:25:31 +0800

    Update generate_proxies.py

--- generate_proxies.py
+++ generate_proxies.py
@@ -10,8 +10,8 @@ import json
 FILE_URLS = {
    # 'all_unique_nodes': 'https://raw.githubusercontent.com/qjlxg/HA/refs/heads/main/all_unique_nodes.txt',
    # 'merged_configs': 'https://raw.githubusercontent.com/qjlxg/HA/refs/heads/main/merged_configs.txt',
-   # 'ha_link': 'https://raw.githubusercontent.com/qjlxg/HA/refs/heads/main/link.yaml',
-    'vt_link': 'https://raw.githubusercontent.com/qjlxg/VT/refs/heads/main/link.yaml',
+    'ha_link': 'https://raw.githubusercontent.com/qjlxg/HA/refs/heads/main/link.yaml',
+   # 'vt_link': 'https://raw.githubusercontent.com/qjlxg/VT/refs/heads/main/link.yaml',
   #  '520_link': 'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/520.yaml',
   #  'clash_link': 'https://raw.githubusercontent.com/qjlxg/aggregator/refs/heads/main/data/clash.yaml'
 }
